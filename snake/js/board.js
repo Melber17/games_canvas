79e04db69265ce145e798e7834084cefcf4b5285
Evgeniy <63192915+Melber17@users.noreply.github.com>
2021-03-11 11:11:48 +0300

    finished

--- snake/js/board.js
+++ snake/js/board.js
@@ -23,13 +23,45 @@ game.board = {
             y: offsetY + cellSize * row
         };
     },
+    getRandomAvailableCell() {
+        let pool = this.cells.filter(cell => !cell.type && !this.game.snake.hasCell(cell));
+        let index = this.game.random(0, pool.length - 1);
+        return pool[index];
+    },
+    createCellObject(type) {
+        // получить текущую ячейку с данным объектом
+        let cell = this.cells.find(cell => cell.type === type);
+        if (cell) {
+            cell.type = false;
+        }
+
+        // получить случайную доступную ячейку для нового объекта
+        cell = this.getRandomAvailableCell();
+
+        // установить поле нового объекта
+        cell.type = type;
+    },
+    createFood() {
+        this.createCellObject("food");
+    },
+    createBomb() {
+        this.createCellObject("bomb");
+    },
+    isFoodCell(cell) {
+        return cell.type === "food";
+    },
+    isBombCell(cell) {
+        return cell.type === "bomb";
+    },
     getCell(row, col) {
         return this.cells.find(cell => cell.row === row && cell.col === col);
     },
     render() {
         this.cells.forEach(cell => {
             this.game.ctx.drawImage(this.game.sprites.cell, cell.x, cell.y);
+            if (cell.type) {
+                this.game.ctx.drawImage(this.game.sprites[cell.type], cell.x, cell.y);
+            }
         });
     }
-
 };
